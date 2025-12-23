69698d76895a304ef9d0b59cb14e559b90a1e572
Saloni Maniyar <salonimaniyar16@gmail.com>
2025-12-23 15:31:53 +0530

    fix:resolved prisma setup

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,28 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient implements OnModuleInit,OnModuleDestroy {
+  
 
-  async onModuleDestroy() {
-    await this.$disconnect();
-  }
+    constructor(){
+          console.log('DB URL:', process.env.DATABASE_URL);
+          const databaseUrl = process.env.DATABASE_URL;
+
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+        const adapter=new PrismaPg({
+            connectionString:process.env.DATABASE_URL as string,
+        });
+        super({adapter});
+    }
+    async onModuleInit() {
+        await this.$connect();
+    }
+    async onModuleDestroy() {
+        await this.$disconnect();
+    }
 }
