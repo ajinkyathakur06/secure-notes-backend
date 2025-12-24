6b6bb8f211239080decdac626a0d99801f3e579a
Saloni Maniyar <salonimaniyar16@gmail.com>
2025-12-24 22:14:48 +0530

    feat:User module added for get user details for profile and update profile

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [PrismaModule, AuthModule,
      ConfigModule.forRoot({
       isGlobal: true,
-    }),],
+    }),
+     UserModule,],
   controllers: [AppController],
   providers: [AppService],
 })
