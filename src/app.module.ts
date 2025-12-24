03fb13028900f07868923b53aeb24faa6eec8463
Saloni Maniyar <salonimaniyar16@gmail.com>
2025-12-24 15:47:06 +0530

    feat:Notes Crud completed , Download as text file completed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
   imports: [PrismaModule, AuthModule,
      ConfigModule.forRoot({
       isGlobal: true,
-    }),],
+    }),
+     NotesModule,],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/notes/notes.service.ts
@@ -0,0 +1,192 @@
+import {ForbiddenException, Injectable,NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateNoteDto } from './dto/create_note.dto';
+import { UpdateNoteDto } from './dto/update_note.dto';
+
+@Injectable()
+export class NotesService {
+    getSharedNotes(userId: any) {
+        throw new Error('Method not implemented.');
+    }
+    async getOwnedNotes(userId: string) {
+        return await this.prisma.notes.findMany({
+            where:{
+                user_id:userId,
+                OR:[
+                    {
+                        noteMeta:{
+                            none:{
+                                user_id:userId,
+                            },
+                        },
+                    },
+                    {
+                        noteMeta:{
+                            some:{
+                                user_id:userId,
+                                is_deleted:false,
+
+                            },
+                        },
+                    },
+                ],
+            
+            },
+            orderBy:{
+                updatedAt:'desc',
+            },
+            select:{
+                note_id:true,
+                title:true,
+                updatedAt:true,
+            },
+            
+        });
+       
+    }
+    //download as txt
+    async download(noteId: string, userId: string) {
+        const note = await this.getOne(noteId, userId);
+
+        const text = `Title: ${note.title}\n\n${note.content}`;
+
+        return {
+             filename: `${note.title}.txt`,
+             content: text,
+        };
+    }
+    //delete note
+    async delete(noteId:string,userId:string,scope:'me'|'all'){
+        const note=await this.prisma.notes.findUnique({
+            where:{note_id:noteId},
+        });
+
+        if(!note){
+            throw new NotFoundException('Note not found');
+        }
+
+        //Owner can delete for  everyone
+        if(scope==='all'){
+            if(note.user_id!==userId){
+                throw new ForbiddenException('Only owner can delete note for everyone',);
+            }
+            return this.prisma.notes.delete({
+                where:{note_id:noteId},
+            });
+        }
+
+        //delete for me owner or shared user
+        return this.prisma.userNoteMeta.upsert({
+            where: {
+                user_id_note_id: {
+                    user_id: userId,
+                    note_id: noteId,
+                },
+            },
+            create: {
+                user_id:userId,
+                note_id:noteId,
+                is_deleted:true,
+            },
+            update: {
+                is_deleted:true,
+            }
+        });
+    }
+
+
+    //get note
+    async getOne(noteId: string, userId: any) {
+        const note=await this.prisma.notes.findUnique({
+            where:{note_id:noteId},
+            include:{
+                noteMeta:{
+                    where:{user_id:userId},
+                    select:{is_deleted:true},
+                },
+            },
+        });
+        
+        //if note does not exist 
+        if(!note){
+            throw new NotFoundException('Note not found ');
+        }
+
+        //if user has deleted note
+        if(note.noteMeta.length && note.noteMeta[0].is_deleted){
+            throw new NotFoundException('Note not found ');
+        }
+
+        //owner can view
+        if(note.user_id===userId){
+            return note;
+        }
+
+        //check for shared access for not owner
+        const shared=await this.prisma.request.findFirst({
+            where:{
+                note_id:noteId,
+                receiver_id:userId,
+                status:'ACCEPTED'
+            },
+        });
+        if(!shared){
+            throw new ForbiddenException("You do not have access to this note");
+        }
+        return note;
+    }
+
+    //update
+    async update(noteId: string, userId: any, dto: UpdateNoteDto) {
+        const note=await this.prisma.notes.findUnique({
+            where:{note_id:noteId},
+        });
+        if(!note){
+            throw new NotFoundException('Note not found');
+        }
+
+        //always owner can update
+        if(note.user_id===userId){
+            return this.prisma.notes.update({
+                where:{note_id:noteId},
+                data:{...dto}
+            });
+        }
+
+        //check shared person has edit access
+        const sharedEditAccess=await this.prisma.request.findFirst({
+            where:{
+                note_id:noteId,
+                receiver_id:userId,
+                status:'ACCEPTED',
+                permission:'EDIT',
+            },
+        });
+
+        if(!sharedEditAccess){
+            throw new ForbiddenException('You do not have edit access to this note ');
+        }
+
+        //shared user with edit permission
+        return this.prisma.notes.update({
+            where:{note_id:noteId},
+            data:{...dto,}
+        })
+
+
+    }
+    constructor(private readonly prisma:PrismaService){
+
+    }
+
+    //Create Note
+    async create(userId:string,dto:CreateNoteDto){
+        return this.prisma.notes.create({
+            data:{
+                user_id:userId,
+                title:dto.title,
+                content:dto.content,
+            },
+        });
+    }
+}
