5d9aa81749bc382aecf1ede9064a91c90be990c9
Saloni Maniyar <116364523+Saloni-Maniyar@users.noreply.github.com>
2025-12-28 19:24:33 +0530

    Uncomment AuthModule import in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-//import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { ExtractorModule } from './extractor/extractor.module';
 
